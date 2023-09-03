22fe5f89f9c80ae1de99afc133d65c16dc2bb174
Umair <umairwasim001@gmail.com>
2023-09-03 17:08:06 +0500

    Create Launcher.cs

---
+++ PlatformShooterMultiplayer/Assets/Scripts/Launcher.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+using Photon.Realtime;
+using System.Linq;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public static Launcher Instance;
+
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
+    [SerializeField] GameObject startGameButton;
+    [SerializeField] Transform roomListContent;
+    [SerializeField] Transform playerListContent;
+    [SerializeField] RoomListItem roomListItemPrefab;
+    [SerializeField] PlayerListItem PlayerListItemPrefab;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        Debug.Log("Connecting to Master");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public void CreateRoom()
+    {
+        //Prevent null or empty room name field for Room Creation
+        if (string.IsNullOrEmpty(roomNameInputField.text))
+            return;
+
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.Instance.SwtichMenu(MenuType.Loading);
+    }
+
+
+    #region Function Overrides
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+        PhotonNetwork.JoinLobby();
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+        MenuManager.Instance.SwtichMenu(MenuType.Title);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.Instance.SwtichMenu(MenuType.Room);
+        roomNameText.text = "Room Name: " + PhotonNetwork.CurrentRoom.Name;
+
+        Player[] players = PhotonNetwork.PlayerList;
+
+        //Delete previous content in children
+        foreach (Transform child in playerListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        //set up players
+        for (int i = 0; i < players.Count(); i++)
+        {
+            Instantiate(PlayerListItemPrefab, playerListContent).SetUp(players[i]);
+            //if (playerItem.TryGetComponent(out PlayerListItem playerListItem))
+            //{
+            //    playerListItem.SetUp(players[i]);
+            //}
+
+        }
+
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        errorText.text = "Room Creation Failed: " + message;
+        MenuManager.Instance.SwtichMenu(MenuType.Error);
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.SwtichMenu(MenuType.Title);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (Transform trans in roomListContent)
+        {
+            Destroy(trans.gameObject);
+        }
+
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            if (roomList[i].RemovedFromList)
+                continue;
+
+            Instantiate(roomListItemPrefab, roomListContent).SetUp(roomList[i]);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Instantiate(PlayerListItemPrefab, playerListContent).SetUp(newPlayer);
+    }
+
+    #endregion
+
+    public void StartGame()
+    {
+        PhotonNetwork.LoadLevel(1);
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.SwtichMenu(MenuType.Loading);
+    }
+
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+        MenuManager.Instance.SwtichMenu(MenuType.Loading);
+    }
+
+
+}
