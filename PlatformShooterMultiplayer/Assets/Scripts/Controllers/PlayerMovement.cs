ffa614a5efb1a2f33504401cf2e67b55323956f0
Umair <umairwasim001@gmail.com>
2023-09-03 17:08:33 +0500

    Create PlayerMovement.cs

---
+++ PlatformShooterMultiplayer/Assets/Scripts/Controllers/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerController playerController;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+
+    private float horizontalMove = 0f;
+    private bool jump = false;
+    private bool crouch = false;
+
+    private const string HORIZONTAL = "Horizontal";
+    private const string JUMP = "Jump";
+    private const string CROUCH = "Crouch";
+    
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw(HORIZONTAL) * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown(JUMP))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+             }
+
+
+    void FixedUpdate()
+    {
+        // Move our character
+        //playerController.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        //jump = false;
+    }
+}
