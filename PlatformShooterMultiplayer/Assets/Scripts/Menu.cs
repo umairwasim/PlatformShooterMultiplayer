a4267d3540ce92429b2751e28bfd308d66ef6cf6
Umair <umairwasim001@gmail.com>
2023-09-03 17:07:28 +0500

    Create Menu.cs

---
+++ PlatformShooterMultiplayer/Assets/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MenuType { Loading, Title, CreateRoom, Room, Error, FindRoom }
+
+public class Menu : MonoBehaviour
+{
+	public MenuType menuType;
+    private bool active;
+
+	public bool IsActive() => active = true;
+
+    public void Open() => gameObject.SetActive(active = true);
+
+    public void Close() => gameObject.SetActive(active = false);
+}
