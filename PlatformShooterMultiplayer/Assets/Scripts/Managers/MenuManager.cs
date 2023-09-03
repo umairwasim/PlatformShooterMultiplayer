fadb1f5effef19bce7111a800d110e65946d2cd8
Umair <umairwasim001@gmail.com>
2023-09-03 17:07:58 +0500

    Create MenuManager.cs

---
+++ PlatformShooterMultiplayer/Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance;
+
+    [SerializeField] private Menu[] menus;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void SwtichMenu(MenuType menuType)
+    {
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].menuType == menuType)
+            {
+                menus[i].Open();
+            }
+            else if (menus[i].IsActive())
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+    }
+
+    public void OpenMenu(Menu menu)
+    {
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].IsActive())
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+        menu.Open();
+    }
+
+    public void CloseMenu(Menu menu)
+    {
+        menu.Close();
+    }
+
+    public void ApplicationQuit()
+    {
+        Application.Quit();
+    }
+}
