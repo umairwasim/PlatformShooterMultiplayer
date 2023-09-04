51490370a705ae5b663020cccd677c0d26c75413
Umair <umairwasim001@gmail.com>
2023-09-04 20:16:42 +0500

    Update Menu.cs

--- PlatformShooterMultiplayer/Assets/Scripts/Menu.cs
+++ PlatformShooterMultiplayer/Assets/Scripts/Menu.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public enum MenuType { Loading, Title, CreateRoom, Room, Error, FindRoom }
 
 public class Menu : MonoBehaviour
 {
-	public MenuType menuType;
+    public MenuType menuType;
+  
     private bool active;
+    private readonly float scaleMultipler = 0.5f;
+    private readonly float scaleDuration = 0.25f;
+    public bool IsActive() => active = true;
 
-	public bool IsActive() => active = true;
+    public void Open()
+    {
+        gameObject.SetActive(IsActive());
+        transform.DOPunchScale(Vector3.one * scaleMultipler, scaleDuration);
+    }
 
-    public void Open() => gameObject.SetActive(active = true);
-
-    public void Close() => gameObject.SetActive(active = false);
+    public void Close() => gameObject.SetActive(!IsActive());
 }
